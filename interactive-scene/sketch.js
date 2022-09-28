523f185aad5abc51f57d6e5d55563f020d07858c
daveiscool96 <114016287+daveiscool96@users.noreply.github.com>
2022-09-28 11:20:17 -0600

    Update sketch.js

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -1,15 +1,31 @@
 // Interactive-Scene
-// Your Name
-// Date
+// Dave H.
+// 9/22/2022
 //
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+let monke;
+let scalar = 2;
+
+function preload(){
+  monke = loadImage("monke.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  imageMode(CENTER);
 }
 
 function draw() {
-  background(220);
+  //background(220);
+  image(monke, mouseX, mouseY, monke.width*scalar, monke.height*scalar);
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    scalar = scalar * 1.5;
+  } else if (keyCode === DOWN_ARROW) {
+    scalar = scalar * 0.75;
+  }
 }
