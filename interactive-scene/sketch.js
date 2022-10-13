6613968d0cfde2b7d89a2fbc6cd8ee2226ceb0ce
daveiscool96 <114016287+daveiscool96@users.noreply.github.com>
2022-10-13 17:18:05 -0600

    shtuff

--- interactive-scene/sketch.js
+++ interactive-scene/sketch.js
@@ -5,8 +5,35 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
+//interactive background
 let spiberBG;
 
+//below sprites for character state machine
+
+//idol right
+let spiberIdolRight
+
+//right
+let spiberR1
+let spiberR2
+let spiberR3
+let spiberR4
+let spiberR5
+let spiberR6
+
+//idol wall right
+let spiberWallR1
+
+// wall right
+
+
+//left
+
+//idol left
+
+//idol wall left
+
+//wall left
 function preload() {
   spiberBG = loadImage("buildingsBackground.png");
 }
