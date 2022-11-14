514a78f7e4b13695ed87362afbdc7c61add655f0
daveiscool96 <114016287+daveiscool96@users.noreply.github.com>
2022-11-14 11:58:01 -0600

    fdsGH

---
+++ walker oop/sketch.js
@@ -0,0 +1,63 @@
+// Project Title
+// Your Name
+// Date
+//
+// Extra for Experts:
+// - describe what you did to take this project "above and beyond"
+
+class Walker {
+  constructor(x, y) {
+    this.x = x;
+    this.y = y;
+    this.color = "brown";
+    this.speed = 5;
+    this. radius = 50;
+  }
+
+  display() {
+    stroke("black");
+    fill(this.color);
+    circle(this.x, this.y, this. radius * 2);
+  }
+
+  move() {
+    let choice = random(100);
+    
+    if (choice < 25) {
+      this.y -= this.speed;
+    }
+    else if (choice < 50) {
+      this.y += this.speed;
+    }
+    else if (choice < 75) {
+      this.x += this.speed;
+    }
+    else {
+      this.x -= this.speed;
+    }
+  }
+}
+
+let mike;
+let kate;
+let Hitler;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  mike = new Walker(width/2, height/2);
+  kate = new Walker(width/2, height/2);
+  Hitler = new Walker(width/2, height/2);
+}
+
+function draw() {
+  kate.color = "blue";
+  Hitler.color = "white";
+  Hitler.display();
+  mike.display();
+  kate.display();
+  Hitler.move();
+  mike.move();
+  kate.move();
+}
+
+
